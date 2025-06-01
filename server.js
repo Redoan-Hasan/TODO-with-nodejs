8454cb26c2ebad42e8f1c4d45c6b2c21303105aa
Redoan Hasan <redoanhasan57@gmail.com>
2025-06-01 08:41:57 +0600

    fetching single todo done

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
-const { create } = require('domain');
+
 
 const filePath = path.join(__dirname, "/db/db.json")
 const server = http.createServer((req,res)=>{
+    const url = new URL(req.url,`http://${req.headers.host}`);
+    const pathname = url.pathname;
+    // console.log(url.pathname);
+
+
     //getting all the todos
-    if(req.url === "/todos" && req.method === "GET"){
+    if(pathname === "/todos" && req.method === "GET"){
         const data = fs.readFileSync(filePath, {encoding:"utf-8"});
         res.writeHead(200,{
             "content-type" : "application/json"
@@ -19,7 +24,7 @@ const server = http.createServer((req,res)=>{
     }
 
     //creating a todo
-    else if(req.url === "/todos/create" && req.method === "POST"){
+    else if(pathname === "/todos/create" && req.method === "POST"){
         let data = "";
 
         req.on("data",(chunk)=>{
@@ -41,6 +46,15 @@ const server = http.createServer((req,res)=>{
         fs.writeFileSync(filePath, JSON.stringify(allTodos,null,2), {encoding: "utf-8"});
         res.end(JSON.stringify({title,body,createdAt},null,2));
     });
+    }
+    //single todo
+    else if(pathname === "/todo" && req.method === "GET"){
+        const title = url.searchParams.get("title");
+        // console.log(title);
+        const allTodos = JSON.parse(fs.readFileSync(filePath, {encoding: "utf-8"}));
+        const singleTodo = allTodos.find((todo) => todo.title === title);
+        console.log(singleTodo);
+        res.end(JSON.stringify(singleTodo,null,2));
     }
     else{
         res.end("Route not found");
